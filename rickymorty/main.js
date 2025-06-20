2a499e776c01b1dfc8881ebeac162cb5dc997e24
prueba1 <marcoariasmedrano@hotmail.com>
2025-06-20 12:23:13 -0500

    Prueba2

--- rickymorty/main.js
+++ rickymorty/main.js
@@ -1,5 +1,5 @@
 let contenedor = document.querySelector("#contenedor-cards");
-let pokeNumeros = 1; // Empieza desde el personaje 1
+let PersonajesNumeros = 1; // Empieza desde el personaje 1
 
 async function traerPersonajes(offset = 1) {
   try {
@@ -10,23 +10,24 @@ async function traerPersonajes(offset = 1) {
       personajes.push(response.data);
     }
 
-    renderizarPokes(personajes);
+    renderizarPersonajes(personajes);
   } catch (error) {
     console.log("Error al traer personajes:", error);
   }
 }
 
-function renderizarPokes(personajes) {
+function renderizarPersonajes(personajes) {
   contenedor.innerHTML = "";
   personajes.forEach((personaje) => {
     contenedor.innerHTML += `
-      <div class="card flex flex-col items-center justify-center">
-        <div id="cont-img" class="bg-gray-100 rounded-xl">
-          <img src="${personaje.image}" alt="${personaje.name}" />
+      <div class="card bg-slate-600 max-w-xs shadow-lg rounded-xl overflow-hidden transition-transform hover:scale-105 duration-300 p-2">
+        <div class="w-full h-56 overflow-hidden bg-gray-200">
+          <img src="${personaje.image}" alt="${personaje.name}" class="w-full h-full object-cover rounded-4xl" />
         </div>
-        <div id="cont-pkm" class="flex flex-col justify-start w-full p-2 gap-1">
-          <h3 class="text-gray-500 font-semibold">#${String(personaje.id).padStart(4, '0')}</h3>
-          <h2 class="text-2xl">${personaje.name}</h2>
+        <div class="p-4 flex flex-col items-start gap-2">
+          <h3 class="text-sm text-gray-400 font-semibold tracking-wide">#${String(personaje.id).padStart(4, '0')}</h3>
+          <h2 class="text-xl font-bold text-white">${personaje.name}</h2>
+          <span class="text-sm font-medium px-2 py-1 bg-blue-100 text-blue-800 rounded-full">${personaje.status}</span>
         </div>
       </div>
     `;
@@ -38,16 +39,16 @@ let ant = document.querySelector("#ant");
 let sig = document.querySelector("#sig");
 
 sig.addEventListener("click", function () {
-  if (pokeNumeros + 12 <= 826) { // Hay 826 personajes en total
-    pokeNumeros += 12;
-    traerPersonajes(pokeNumeros);
+  if (PersonajesNumeros + 12 <= 826) { // Hay 826 personajes en total
+    PersonajesNumeros += 12;
+    traerPersonajes(PersonajesNumeros);
   }
 });
 
 ant.addEventListener("click", function () {
-  if (pokeNumeros > 1) {
-    pokeNumeros -= 12;
-    traerPersonajes(pokeNumeros);
+  if (PersonajesNumeros > 1) {
+    PersonajesNumeros -= 12;
+    traerPersonajes(PersonajesNumeros);
   }
 });
 
